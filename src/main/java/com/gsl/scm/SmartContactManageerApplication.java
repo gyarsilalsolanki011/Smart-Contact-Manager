77ce30216c9303a020f1035cd979d49c2d6d4344
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-08-15 17:49:21 +0530

    First commit

---
+++ src/main/java/com/gsl/scm/SmartContactManageerApplication.java
@@ -0,0 +1,13 @@
+package com.gsl.scm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContactManageerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartContactManageerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gsl/scm/SmartContactManageerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsl.scm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContactManageerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

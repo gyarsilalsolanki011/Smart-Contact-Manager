f04b167299c4c85a2d27f5efe2ceabbce76737ca
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-08-15 20:05:35 +0530

    web pages Designed with html, css, js

---
+++ src/main/java/com/gsl/scm/controller/PageController.java
@@ -0,0 +1,51 @@
+package com.gsl.scm.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class PageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/home";
+    }
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        log.info("Home page handler");
+        model.addAttribute("name", "Substring Technologies");
+        model.addAttribute("youtubeChannel", "Just love coding");
+        model.addAttribute("githubRepo", "https://github.com/learncodewithdurgesh/");
+        return "home";
+    }
+
+    @RequestMapping("/about")
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", true);
+        log.info("About page loading");
+        return "about";
+    }
+
+
+    @RequestMapping("/services")
+    public String servicesPage() {
+        log.info("services page loading");
+        return "services";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+}

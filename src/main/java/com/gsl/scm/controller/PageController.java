7239ea98fbeab904524ed44bbdac5107efbd4e27
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-08-23 04:33:21 +0530

    Some EndPoints created

--- src/main/java/com/gsl/scm/controller/PageController.java
+++ src/main/java/com/gsl/scm/controller/PageController.java
@@ -1,15 +1,29 @@
 package com.gsl.scm.controller;
 
+import com.gsl.scm.entities.User;
+import com.gsl.scm.forms.UserForm;
+import com.gsl.scm.helper.Message;
+import com.gsl.scm.helper.MessageType;
+import com.gsl.scm.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @Slf4j
 public class PageController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/")
     public String index() {
@@ -47,5 +61,42 @@ public class PageController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+
+        UserForm userForm = new UserForm();
+        model.addAttribute("userForm", userForm);
+        return "register";
+    }
+
+    @RequestMapping(value = "/do-register", method = RequestMethod.POST)
+    public String processRegister(@Valid @ModelAttribute UserForm userForm, BindingResult rBindingResult,
+                                  HttpSession session) {
+        System.out.println("Processing registration");
+        System.out.println(userForm);
+
+        // validate form data
+        if (rBindingResult.hasErrors()) {
+            return "register";
+        }
+
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setAbout(userForm.getAbout());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setEnabled(false);
+        user.setProfilePic(
+                "https://www.learncodewithdurgesh.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdurgesh_sir.35c6cb78.webp&w=1920&q=75");
+
+        userService.saveUser(user);
+        System.out.println("user saved :");
+        Message message = Message.builder().content("Registration Successful").type(MessageType.green).build();
+
+        session.setAttribute("message", message);
+        return "redirect:/register";
     }
 }
